4fc4eba798164528c14d217e6a696747fad08b99
Randah <randah.Almajnooni.jed.k1@tuwaiq.edu.sa>
2021-12-18 17:10:16 +0300

    layout set up

---
+++ app/src/main/java/com/tuwiaq/projectgame/data/FlickerApi.kt
@@ -0,0 +1,4 @@
+package com.tuwiaq.projectgame.data
+
+interface FlickerApi {
+}
--- app/src/main/java/com/tuwiaq/projectgame/ui/MainFragment.kt
+++ app/src/main/java/com/tuwiaq/projectgame/ui/MainFragment.kt
@@ -1,10 +1,11 @@
-package com.tuwiaq.projectgame
+package com.tuwiaq.projectgame.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.tuwiaq.projectgame.R
 
 class MainFragment : Fragment() {
 
